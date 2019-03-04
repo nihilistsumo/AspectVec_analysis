f91a492f6676f51fa00bf25fbfedd566240f6e90
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-03-04 15:41:58 -0500

    multithreaded global para vec

--- src/aspvec_dim_red.py
+++ src/aspvec_dim_red.py
@@ -5,13 +5,6 @@ from concurrent import futures
 import numpy as np
 from collections import Counter
 
-def print_para_coverage(paras, asps, sorted_aspids, useful_aspids, c):
-    para_coverage = []
-    for a in asps:
-        para_coverage.append(len(set(a).intersection(set(useful_aspids))))
-    para_coverage.sort()
-    print(str(len(paras)) + ":" + str(len(sorted_aspids)) + ":" + str(c) + ":" + str(para_coverage[:3]))
-
 def project_para_global_asp(p, paras, asps, aspvals, useful_asps, result_dict):
     uvals = []
     for uasp in range(len(useful_asps)):
@@ -22,35 +15,6 @@ def project_para_global_asp(p, paras, asps, aspvals, useful_asps, result_dict):
             uvals.append(0.0)
     result_dict[paras[p]] = uvals
     print(".")
-
-def get_useful_aspval_mat(paras, asps, aspvals, k):
-    # create frequency dictionary of asp ids
-    # take top asps
-    # create aspval matrix corresponding to top asps
-    # do eigen analysis on top of that
-    asps_freq = Counter(asps.flatten())
-    sorted_aspids = sorted(asps_freq, key=asps_freq.get, reverse=True)
-    c = 0
-    # k = (cut*100)/(len(paras), only those asps will remain whose para-frequency is at least k% of len(paras)
-    cut = len(paras)*k//100 + 1
-    for sa in sorted_aspids:
-        if asps_freq[sa] < cut:
-            break
-        c+=1
-    useful_aspids = sorted_aspids[:c]
-    print_para_coverage(paras, asps, sorted_aspids, useful_aspids, c)
-    print("Creating expanded data matrix")
-    useful_aspvals_dict = dict()
-
-    executor = concurrent.futures.ProcessPoolExecutor(10)
-    futures = [executor.submit(project_para_global_asp, p, paras, asps, aspvals, useful_aspids, useful_aspvals_dict) for p in range(len(paras))]
-    concurrent.futures.wait(futures)
-
-    # now useful_aspvals has asp values for each paras in row and each useful_asp in column
-    useful_aspvals = []
-    for p in range(len(paras)):
-        useful_aspvals.append(useful_aspvals_dict[paras[p]])
-    return np.array(useful_aspids), np.array(useful_aspvals)
 
 def eigen_analysis(data_matrix):
     means = np.mean(data_matrix.T, axis=1)
@@ -63,7 +27,6 @@ def eigen_analysis(data_matrix):
 def usage():
     print("parameters: art-qrels paras-file.npy aspids-file.npy aspvals-file.npy output-dir k(in %)")
 
-
 if len(sys.argv) < 7:
     usage()
     sys.exit()
@@ -73,27 +36,19 @@ aspids_file = sys.argv[3]
 aspvals_file = sys.argv[4]
 outdir = sys.argv[5]
 k = int(sys.argv[6])
-page_para_dict = dict()
-with open(art_qrels,'r') as art:
-    for line in art:
-        page = line.split(" ")[0]
-        para = line.split(" ")[2]
-        if page in page_para_dict.keys():
-            page_para_dict[page].append(para)
-        else:
-            page_para_dict[page] = [para]
+
 paras_np = np.load(paras_file)
 aspids_np = np.load(aspids_file)
 aspvals_np = np.load(aspvals_file)
-useful_asps, aspval_matrix = get_useful_aspval_mat(paras_np, aspids_np, aspvals_np, k)
-print("Going to do eigen analysis of "+str(len(paras_np))+" paras each of "+str(len(aspval_matrix[0]))+" dimensions")
-proj_data, eigvals, eigvecs = eigen_analysis(aspval_matrix)
-print("Done, going to save")
-np.save(outdir+"/useful_aspids", useful_asps)
-np.save(outdir+"/projected-data", proj_data)
-np.save(outdir+"/eigvals", eigvals)
-np.save(outdir+"/eigvecs", eigvecs)
-print("Saved")
+
+# print("Going to do eigen analysis of "+str(len(paras_np))+" paras each of "+str(len(aspval_matrix[0]))+" dimensions")
+# proj_data, eigvals, eigvecs = eigen_analysis(aspval_matrix)
+# print("Done, going to save")
+# np.save(outdir+"/useful_aspids", useful_asps)
+# np.save(outdir+"/projected-data", proj_data)
+# np.save(outdir+"/eigvals", eigvals)
+# np.save(outdir+"/eigvecs", eigvecs)
+# print("Saved")
 
 
 # for page in page_para_dict.keys():
