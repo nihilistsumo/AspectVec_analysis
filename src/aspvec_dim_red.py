f024849664474a5f5cedea0e1bebf69aaa61a2ab
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-03-06 21:44:20 -0500

    only save cov matrix

--- src/aspvec_dim_red.py
+++ src/aspvec_dim_red.py
@@ -5,7 +5,7 @@ from concurrent import futures
 import numpy as np
 from collections import Counter
 
-def load_global_data_matrix():
+def load_global_data_matrix_slow():
     data = np.load(indir+"/"+paras_np[0]+".npy")
     c = 1
     for p in range(1, len(paras_np)):
@@ -14,11 +14,20 @@ def load_global_data_matrix():
             para_vec = np.load(indir+"/"+para+".npy")
             data = np.vstack((data, para_vec))
             c+=1
-            print(str(c) + "Data size: "+str(sys.getsizeof(data)/(1024.0*1024.0))+" MB")
+            #print(str(c) + "Data size: "+str(sys.getsizeof(data)/(1024.0*1024.0))+" MB")
         else:
             print(para+" not present in global vector dir")
     return data
 
+def load_global_data_matrix_fast():
+    data = []
+    for p in range(len(paras_np)):
+        para = paras_np[p]
+        if os.path.isfile(indir + "/" + para + ".npy"):
+            data.append(np.load(indir+"/"+para+".npy"))
+        else:
+            print(para+" not present in global vector dir")
+    return np.array(data)
 
 def project_para_global_asp(p, paras, asps, aspvals, useful_asps, result_dict):
     uvals = []
@@ -43,13 +52,15 @@ def eigen_analysis(data_matrix):
     print("Calculating covariance matrix...", end=' ')
     cov_cent_data = np.cov(cent_data.T)
     print("Done")
-    print("Performing eigenvalue decomposition...", end=' ')
-    vals, vecs = np.linalg.eig(cov_cent_data)
-    print("Done")
-    print("Projecting data in transformed space...", end=' ')
-    proj_data = vecs.T.dot(cov_cent_data.T)
-    print("Done")
-    return proj_data, vals, vecs
+    np.save(outdir+"/cov-matrix", cov_cent_data)
+    print("Saved covariance matrix")
+    # print("Performing eigenvalue decomposition...", end=' ')
+    # vals, vecs = np.linalg.eig(cov_cent_data)
+    # print("Done")
+    # print("Projecting data in transformed space...", end=' ')
+    # proj_data = vecs.T.dot(cov_cent_data.T)
+    # print("Done")
+    # return proj_data, vals, vecs
 
 def usage():
     print("parameters: art-qrels paras-file.npy global-para-vecs-dir output-dir")
@@ -64,17 +75,18 @@ outdir = sys.argv[4]
 
 paras_np = np.load(paras_file)
 print("Loading global para vecs")
-aspval_matrix = load_global_data_matrix()
+aspval_matrix = load_global_data_matrix_fast()
 print("Done loading")
 
 
 print("Going to do eigen analysis of "+str(len(paras_np))+" paras each of "+str(len(aspval_matrix[0]))+" dimensions\n")
-proj_data, eigvals, eigvecs = eigen_analysis(aspval_matrix)
-print("Done, going to save")
-np.save(outdir+"/projected-data", proj_data)
-np.save(outdir+"/eigvals", eigvals)
-np.save(outdir+"/eigvecs", eigvecs)
-print("Saved")
+eigen_analysis(aspval_matrix)
+# proj_data, eigvals, eigvecs = eigen_analysis(aspval_matrix)
+# print("Done, going to save")
+# np.save(outdir+"/projected-data", proj_data)
+# np.save(outdir+"/eigvals", eigvals)
+# np.save(outdir+"/eigvecs", eigvecs)
+# print("Saved")
 
 
 # for page in page_para_dict.keys():
