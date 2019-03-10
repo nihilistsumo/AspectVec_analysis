50121604037cc27f2a737f084593f530d025246e
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-03-10 00:34:12 -0500

    add sklearn pca

--- src/aspvec_dim_red.py
+++ src/aspvec_dim_red.py
@@ -6,6 +6,8 @@ import numpy as np
 from collections import Counter
 from scipy import sparse
 from scipy.sparse.linalg import svds
+import sklearn
+from sklearn.decomposition import PCA
 
 def load_global_data_matrix_slow():
     data = np.load(indir+"/"+paras_np[0]+".npy")
@@ -42,8 +44,16 @@ def project_para_global_asp(p, paras, asps, aspvals, useful_asps, result_dict):
     result_dict[paras[p]] = uvals
     print(".")
 
-def eigen_analysis(data_matrix):
-    print("Eigen analysis")
+def pca_sklearn(data_matrix, svdk):
+    pca = PCA(n_components=svdk)
+    pca.fit(data_matrix)
+    print("Sorted explained variance")
+    print(pca.explained_variance_ratio_)
+    print("Total variance explained by pca components: "+str(np.sum(pca.explained_variance_ratio_)))
+    return pca.transform(data_matrix)
+
+def svd_analysis(data_matrix, svdk):
+    print("SVD analysis")
     print("--------------\n")
     print("Calculating col means...")
     means = np.mean(data_matrix.T, axis=1)
@@ -53,34 +63,45 @@ def eigen_analysis(data_matrix):
     print("Done")
     cent_data_sp = sparse.csr_matrix(cent_data)
     print("Calculating svd...")
-    u, s, vt = svds(cent_data_sp.T, k=10)
+    u, s, vt = svds(cent_data_sp.T, k=svdk)
     print("Done")
-    np.save(outdir+"/u-matrix-svd", u)
-    np.save(outdir+"/singular_val_svd", s)
-    np.save(outdir + "/vt-matrix-svd", vt)
-    # print("Calculating covariance matrix...")
-    # cov_cent_data = np.cov(cent_data.T)
-    # print("Done")
-    # np.save(outdir+"/cov-matrix", cov_cent_data)
-    # print("Saved covariance matrix")
-    # print("Performing eigenvalue decomposition...")
-    # vals, vecs = np.linalg.eig(cov_cent_data)
-    # print("Done")
-    # print("Projecting data in transformed space...")
-    # proj_data = vecs.T.dot(cov_cent_data.T)
-    # print("Done")
-    # return proj_data, vals, vecs
+    return u, s, vt
+
+def eigen_analysis(data_matrix):
+    print("Eigen analysis")
+    print("--------------\n")
+    print("Calculating col means...")
+    means = np.mean(data_matrix.T, axis=1)
+    print("Done")
+    print("Centering data matrix...")
+    cent_data = data_matrix - means
+    print("Done")
+
+    print("Calculating covariance matrix...")
+    cov_cent_data = np.cov(cent_data.T)
+    print("Done")
+    np.save(outdir+"/cov-matrix", cov_cent_data)
+    print("Saved covariance matrix")
+    print("Performing eigenvalue decomposition...")
+    vals, vecs = np.linalg.eig(cov_cent_data)
+    print("Done")
+    print("Projecting data in transformed space...")
+    proj_data = vecs.T.dot(cov_cent_data.T)
+    print("Done")
+    return proj_data, vals, vecs
 
 def usage():
-    print("parameters: art-qrels paras-file.npy global-para-vecs-dir output-dir")
+    print("parameters: svd/eig/pca art-qrels paras-file.npy global-para-vecs-dir output-dir svdk")
 
-if len(sys.argv) < 5:
+if len(sys.argv) < 6:
     usage()
     sys.exit()
-art_qrels = sys.argv[1]
-paras_file = sys.argv[2]
-indir = sys.argv[3]
-outdir = sys.argv[4]
+m = sys.argv[1]
+art_qrels = sys.argv[2]
+paras_file = sys.argv[3]
+indir = sys.argv[4]
+outdir = sys.argv[5]
+k = sys.argv[6]
 
 paras_np = np.load(paras_file)
 print("Loading global para vecs")
@@ -88,15 +109,20 @@ aspval_matrix = load_global_data_matrix_fast()
 print("Done loading")
 
 
-print("Going to do eigen analysis of "+str(len(paras_np))+" paras each of "+str(len(aspval_matrix[0]))+" dimensions\n")
-eigen_analysis(aspval_matrix)
-# proj_data, eigvals, eigvecs = eigen_analysis(aspval_matrix)
-# print("Done, going to save")
-# np.save(outdir+"/projected-data", proj_data)
-# np.save(outdir+"/eigvals", eigvals)
-# np.save(outdir+"/eigvecs", eigvecs)
-# print("Saved")
-
+print("Going to do svd/eigen/pca analysis of "+str(len(paras_np))+" paras each of "+str(len(aspval_matrix[0]))+" dimensions\n")
+if m == 'svd':
+    u, s, vt = svd_analysis(aspval_matrix, k)
+    np.save(outdir + "/u-matrix-svd", u)
+    np.save(outdir + "/singular_val_svd", s)
+    np.save(outdir + "/vt-matrix-svd", vt)
+elif m == 'eig':
+    proj_data, eigvals, eigvecs = eigen_analysis(aspval_matrix)
+    np.save(outdir+"/eig-projected-data", proj_data)
+    np.save(outdir+"/eigvals", eigvals)
+    np.save(outdir+"/eigvecs", eigvecs)
+elif m == 'pca':
+    proj_data = pca_sklearn(aspval_matrix, k)
+    np.save(outdir + "/pca-projected-data", proj_data)
 
 # for page in page_para_dict.keys():
 #     page_paras = page_para_dict[page]
